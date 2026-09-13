69e52297db0f5aae8c81749dfb82786805492d0f
Marvelous Akpotu <still4marvelous@gmail.com>
2026-09-13 17:51:56 +0100

    Fix bullmq lint error

--- apps/api/src/types/bullmq.d.ts
+++ apps/api/src/types/bullmq.d.ts
@@ -1,7 +1,7 @@
 declare module 'bullmq' {
   export class Queue {
-    constructor(name: string, opts?: any);
-    getJobs(): Promise<any[]>;
+    constructor(name: string, opts?: unknown);
+    getJobs(): Promise<unknown[]>;
     // Add any other methods you need for tests
   }
 }
